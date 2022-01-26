45870f7a64fa83f7e4c4b75834feaf9c7f228bb1
unknown <egor1@mail.ru>
2022-01-26 17:22:08 +0300

    Add model Tg_user

--- dvmn_projects/project_organizer/models.py
+++ dvmn_projects/project_organizer/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Tg_user(models.Model):
+    tg_id = models.IntegerField()
+    tg_name = models.CharField(max_length=25)
+
+    def __str__(self):
+        return '{} - {}'.format(self.tg_id, self.tg_name)
